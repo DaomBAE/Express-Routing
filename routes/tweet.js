51f6b8195fd5254c69f8a91fe3b00604b5f421c8
DaomBae <da6970@gmail.com>
2023-04-11 11:28:02 +0900

    first commit

---
+++ routes/tweet.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send("트윗 조회");
+});
+
+router.get('/:id', (req, res) => {
+    console.log(req.params);
+    
+    res.send("특정 트윗 조회");
+});
+
+router.post("/", (req, res) => {
+    res.send("트윗 생성");
+  });
+  
+  router.put("/:id", (req, res) => {
+    res.send("특정 트윗 수정");
+  });
+  
+  router.delete("/:id", (req, res) => {
+      res.send("트윗 삭제");
+    });
+
+module.exports = router;
